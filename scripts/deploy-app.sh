f3b2a9c648a7442a0f7d70ae49aebeed8d83a11c
Tom Marvin <tommarvin2002@gmail.com>
2021-04-11 15:25:35 +0000

    minor changes

--- scripts/deploy-app.sh
+++ scripts/deploy-app.sh
@@ -1,4 +1,4 @@
-scp -i ~/.ssh/ansible_id_rsa docker-compose.yaml pc@project-vm:/home/pc/Project-Folder/docker-compose.yaml
-ssh -i ~/.ssh/absible_id_rsa pc@project-vm << EOF
+scp -i ~/.ssh/id_rsa docker-compose.yaml pc@project-vm2:/home/pc/Project-Folder/docker-compose.yaml
+ssh -i ~/.ssh/id_rsa pc@project-vm2 << EOF
     docker stack deploy --compose-file /home/pc/Project-Folder/docker-compose.yaml 1-service
 EOF
