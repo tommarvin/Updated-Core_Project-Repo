476e659760944fcf8a248d24f6a102446310c459
Tom Marvin <tommarvin2002@gmail.com>
2021-04-12 03:15:44 +0000

    minor

--- deploy-app.sh
+++ deploy-app.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-scp -i ~/.ssh/id_rsa docker-compose.yaml pc@manager:docker-compose.yaml
-ssh -i ~/.ssh/id_rsa pc@manager << EOF
+scp -i ~/.ssh/id_rsa docker-compose.yaml jenkins@manager:docker-compose.yaml
+ssh -i ~/.ssh/id_rsa jenkins@manager << EOF
     docker stack deploy --compose-file docker-compose.yaml project-stack
 EOF
